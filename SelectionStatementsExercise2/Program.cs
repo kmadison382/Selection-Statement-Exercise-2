102d82ace671cd12a8c6c924881f88bee1eae56c
kmadison382 <k.madison.382@gmail.com>
2024-06-12 18:53:04 -0500

    switch statements are fun

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your favorite school subject?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "english":
+                    Console.WriteLine("English? Hey, me too!");
+                    break;
+                case "math":
+                    Console.WriteLine("Soo, I liked math...until it started to look more like English");
+                    break;
+                case "art":
+                    Console.WriteLine("Ah, the arts...underappreciated, aren't they?");
+                    break;
+                case "science":
+                    Console.WriteLine("Science! I love science!");
+                    break;
+                case "gym":
+                case "pe":
+                    Console.WriteLine("Gym? You mean like, running and sweating and dodgeball? No thanks!");
+                    break;
+                default:
+                    Console.WriteLine($"{favSubject}? Oh... I don't think I ever took that class.");
+                    break;
+
+            }
         }
     }
 }
